651755c27de0e7ed469a98362d3c641fb9d84939
Amit Kumar Vishwakarma <akviitian@gmail.com>
2024-12-10 19:51:56 +0530

    Time: 4 ms (84.79%), Space: 43.3 MB (53.16%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public String simplifyPath(String path) {
+        //.. will pop the last
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+
+        for (String component : components) {
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!component.equals(".") && !component.isEmpty()) {
+                stack.push(component);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
