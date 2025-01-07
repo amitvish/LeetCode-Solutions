063f3d789c323625c968c9129f808d932af0ed86
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-01-07 16:57:58 +0530

    Time: 1 ms (85.8%), Space: 42.2 MB (20.75%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,26 +2,19 @@ class Solution {
 
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        //create a matrix
-        int matrix[][] = new int[numRows][numRows];
-        //fill matric
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || i == j) {
-                    matrix[i][j] = 1;
-                } else {
-                    matrix[i][j] = matrix[i - 1][j] + matrix[i - 1][j - 1];
-                }
-            }
-        }
 
         for (int i = 0; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j <= i; j++) {
-                row.add(matrix[i][j]);
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
             }
             result.add(row);
         }
+
         return result;
     }
 }
