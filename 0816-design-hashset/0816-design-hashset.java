d1699f48832705938bbf3b1ff16b49990ca8a1ce
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-05-04 17:18:38 +0530

    Time: 17 ms (39.85%), Space: 55.1 MB (16.46%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.java
+++ 0816-design-hashset/0816-design-hashset.java
@@ -1,5 +1,31 @@
-class MyHashSet extends HashSet<Integer> {}
+class MyHashSet {
+    int n = 10000;
+    List<Integer>[] arr;
 
+    public MyHashSet() {
+        arr = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new ArrayList<>();
+        }
+    }
+
+    public void add(int key) {
+        int i = key % n;
+        if (!arr[i].contains(key)) {
+            arr[i].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int i = key % n;
+        arr[i].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int i = key % n;
+        return arr[i].contains(key);
+    }
+}
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
