c702b80df58d1bf11530af7702f2b4a67132b20d
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-05-04 18:40:38 +0530

    Time: 34 ms (20.74%), Space: 55.5 MB (13.56%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,7 +1,21 @@
-class MyHashMap extends HashMap<Integer, Integer> {
+class MyHashMap {
+    int arr[];
+
+    public MyHashMap() {
+        arr = new int[1000001];
+        Arrays.fill(arr, -1);
+    }
+
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
 
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        return arr[key];
+    }
+
+    public void remove(int key) {
+        arr[key] = -1;
     }
 }
 /**
