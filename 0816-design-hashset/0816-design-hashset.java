2feda35d0616e4b1873aba2786d45b955a181ee0
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-05-04 12:18:22 +0530

    Time: 13 ms (81.64%), Space: 55.1 MB (16.46%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.java
+++ 0816-design-hashset/0816-design-hashset.java
@@ -1,22 +1,5 @@
-class MyHashSet {
-    private boolean[] hashPool;
+class MyHashSet extends HashSet<Integer> {}
 
-    public MyHashSet() {
-        hashPool = new boolean[1000001];
-    }
-
-    public void add(int key) {
-        hashPool[key] = true;
-    }
-
-    public void remove(int key) {
-        hashPool[key] = false;
-    }
-
-    public boolean contains(int key) {
-        return hashPool[key];
-    }
-}
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
