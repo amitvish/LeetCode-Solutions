4f9f22b384d7a13b2d59ba591de876bf912471fd
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-05-11 16:36:25 +0530

    Time: 123 ms (5.03%), Space: 45.7 MB (7.48%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,35 @@
 class Solution {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //find next greater element of all element of nums2
-        int nextGreater[] = new int[nums2.length];
-        for (int i = 0; i < nums2.length; i++) {
-            nextGreater[i] = -1;
-        }
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = i; j < nums2.length; j++) {
-                if (nums2[j] > nums2[i]) {
-                    nextGreater[i] = nums2[j];
-                    break;
-                }
+        int nextGreaterElement[] = new int[nums2.length];
+
+        Stack<Integer> stack = new Stack<>();
+        //fill nextGreater element array with stack
+        //start from last and go towards first
+        //add index in stack
+        //craete decreaing stack
+        System.out.println("nums2.length = " + nums2.length); // should print 4
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            System.out.println("i: " + i + ", stack: " + stack); // DEBUG
+
+            while (!stack.isEmpty() && nums2[stack.peek()] <= nums2[i]) {
+                stack.pop();
             }
+
+            nextGreaterElement[i] = stack.isEmpty() ? -1 : nums2[stack.peek()];
+
+            stack.push(i);
         }
+        // Map value to its next greater for nums2
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], nextGreaterElement[i]);
+        }
+        // Build result for nums1
         int result[] = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    result[i] = nextGreater[j];
-                    break;
-                }
-            }
+            result[i] = map.get(nums1[i]);
         }
 
         return result;
