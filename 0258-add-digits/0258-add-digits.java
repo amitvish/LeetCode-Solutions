1a86a4ebfee839aa10a234c8723937925e6a4be3
Amit Kumar Vishwakarma <akviitian@gmail.com>
2025-05-12 18:29:35 +0530

    Time: 1 ms (42.53%), Space: 40.7 MB (84.02%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,7 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        //digital root
-        if(num == 0) return 0;
-        return 1 + (num - 1) % 9;
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
     }
 }
